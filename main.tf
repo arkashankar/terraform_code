ed970523526fe004d00269900af9009c5c528a5a
arkashankar <arkashankarpradhan@gmail.com>
2022-05-16 12:20:04 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_KEY_ID"
+  access_key = "${AWS_ACCESS_KEY_ID}"
+  secret_key = "${AWS_SECRET_KEY_ID}"
   region     = "us-west-2"
 }
 variable "server_port" {
